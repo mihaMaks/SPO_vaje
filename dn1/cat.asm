36ef56454192798728a9e4f0855df167734ace57
mihaMaks <maksbertoncelj@gmail.com>
2024-11-10 17:28:56 +0100

    dn1 -> main folder

---
+++ dn1/cat.asm
@@ -0,0 +1,11 @@
+CAT     START 0
+WAIT    TD	    #0
+	    JEQ	    WAIT
+        
+        RD      #0           . Preberemo znak iz vhoda
+        COMP    #0           . Preverimo, ali je konec datoteke
+        JEQ     END          . Če je konec, končamo program
+        WD      #1                   . Drugače izpišemo znak na izhod
+        
+        J       WAIT         . Gremo nazaj na branje
+END     END CAT
